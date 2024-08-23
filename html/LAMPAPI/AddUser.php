31f265554a4a710c45e568258bdbd4219f62d687
tuccr <metuccr@gmail.com>
2024-08-23 15:42:16 -0400

    added AddUser.php, needs testing with frontend

--- html/LAMPAPI/AddUser.php
+++ html/LAMPAPI/AddUser.php
@@ -13,8 +13,8 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
   returnWithError($conn->connect_error);
 } else {
-    $stmt = $conn->prepare("SELECT firstName,lastName FROM Users WHERE login=?");
-    $stmt->bind_param("s", $inData["Login"]);
+    $stmt = $conn->prepare("SELECT (firstName,lastName) FROM Users WHERE login=?");
+    $stmt->bind_param("s", $inData["login"]);
     $stmt->execute();
     $result = $stmt->get_result();
 
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
         returnWithError("User already exists, please try another Username.");
     }
     else {
-        $add = $conn->prepare("INSERT into Users (firstName,lastName,login,password) VALUES (????)");
+        $add = $conn->prepare("INSERT into Users (firstName,lastName,login,password) VALUES (?,?,?,?)");
         $add->bind_param("ssss", $firstName, $lastName, $login, $password);
         $add->execute();
         $add->close();
