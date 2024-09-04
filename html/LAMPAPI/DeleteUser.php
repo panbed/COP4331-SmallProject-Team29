3f64feb4cd1b8dba46dc24aed8354d04dc17815c
Joe <48526917+jblsp@users.noreply.github.com>
2024-09-03 22:09:10 -0400

    deleteuser.php

---
+++ html/LAMPAPI/DeleteUser.php
@@ -0,0 +1,31 @@
+<?php
+
+include "Config.php";
+
+$inData = getRequestInfo();
+
+$userID = $inData["userID"];
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $stmt = $conn->prepare("DELETE FROM Users WHERE ID=?");
+    $stmt->bind_param("d", $userID);
+    if (!$stmt->execute()) {
+        returnWithError("Unable to delete user.");
+    }
+    $stmt->close();
+}
+$conn->close();
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
