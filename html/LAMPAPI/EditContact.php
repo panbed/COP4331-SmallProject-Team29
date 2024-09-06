a0b6c0d39e896340d95227c665a20603d07dae8a
tuccr <metuccr@gmail.com>
2024-09-06 18:40:52 -0400

    EditContact corrected to fit new DB schema

--- html/LAMPAPI/EditContact.php
+++ html/LAMPAPI/EditContact.php
@@ -8,14 +8,18 @@ $id = $inData["id"];
 $name = $inData["name"];
 $phone = $inData["phone"];
 $email = $inData["email"];
+$picture = $inData["picture"];
+$addr = $inData["address"];
+$birthday = $inData["birthday"];
+$notes = $inData["notes"];
 
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
   returnWithError($conn->connect_error);
 } else {
-  $stmt = $conn->prepare("UPDATE Contacts SET Name=?, Phone=?, Email=? WHERE ID=?");
-  $stmt->bind_param("sssd", $name, $phone, $email, $id);
+  $stmt = $conn->prepare("UPDATE Contacts SET Name=?, Phone=?, Email=?, Picture=?, Address=?, Birthday=?, Notes=? WHERE ID=?");
+  $stmt->bind_param("sssssssd", $name, $phone, $email, $picture, $addr, $birthday, $notes, $id);
   if (!$stmt->execute()) {
     returnWithError("Unable to edit contact.");
   }
