7e5e863c8e1ff0387f1cdcde84dd625528f252fa
tuccr <metuccr@gmail.com>
2024-09-26 22:43:44 -0400

    email now required for newuser, regex validation

--- html/LAMPAPI/AddUser.php
+++ html/LAMPAPI/AddUser.php
@@ -8,23 +8,25 @@ $firstName = $inData["firstName"];
 $lastName = $inData["lastName"];
 $login = $inData["login"];
 $password = $inData["password"];
+$email = $inData["email"];
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
 $secure = md5($password);
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
   returnWithError($conn->connect_error);
 } else {
-    $stmt = $conn->prepare("SELECT firstName,lastName FROM Users WHERE login=?");
-    $stmt->bind_param("s", $inData["login"]);
+    $stmt = $conn->prepare("SELECT firstName,lastName FROM Users WHERE login=? OR email=?");
+    $stmt->bind_param("ss", $login, $email);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if($row = $result->fetch_assoc()) {
-        returnWithError("User Already Exists");
+        returnWithError("User with that username or email already exists");
     }
     else {
-        $stmt = $conn->prepare("INSERT into Users (firstName,lastName,Login,Password) VALUES (?,?,?,?)");
-        $stmt->bind_param("ssss", $firstName, $lastName, $login, $secure);
+        $stmt = $conn->prepare("INSERT into Users (firstName,lastName,Email,Login,Password) VALUES (?,?,?,?,?)");
+        $stmt->bind_param("sssss", $firstName, $lastName, $email, $login, $secure);
         $stmt->execute();
         returnWithError("");
     }
